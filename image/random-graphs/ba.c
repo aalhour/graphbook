f584fbf8226fb2ff6203a1fe0af7954029e1e092
Minh Van Nguyen <mvngu.name@gmail.com>
2012-01-02 00:19:47 +1100

    update copyright years

--- image/random-graphs/ba.c
+++ image/random-graphs/ba.c
@@ -1,5 +1,5 @@
 /**************************************************************************
-* Copyright (C) 2011 Minh Van Nguyen <mvngu.name@gmail.com>
+* Copyright (C) 2012 Minh Van Nguyen <mvngu.name@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
